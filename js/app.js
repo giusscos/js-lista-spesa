2a432e5b0d9a158c446b7b0f46222909910bdb6a
giusscos <giu.cosenza08@gmail.com>
2022-07-27 15:48:53 +0000

    Add While Solution

--- js/app.js
+++ js/app.js
@@ -6,8 +6,9 @@ const listArray = [
     'Cipolla 3Kg'
 ];
 const listGroupElement = document.querySelector('.listGroup');
-listGroupElement.innerHTML = '';
+listGroupElement.innerHTML = 'FOR';
 
+// Soluzione con FOR 
 for(let i = 0; i < listArray.length - 1; i++){
     const currentItem = listArray[i];
 
@@ -15,5 +16,19 @@ for(let i = 0; i < listArray.length - 1; i++){
     <li>${currentItem}</li>
     `;
     listGroupElement.innerHTML += listItem;
+}
+
+// Soluzione con WHILE
+listGroupElement.innerHTML += `<br>WHILE`;
+
+let i = 0;
+while(i < listArray.length - 1){
+    const currentItem = listArray[i];
+
+    const listItem = `
+    <li>${currentItem}</li>
+    `;
+    listGroupElement.innerHTML += listItem;
+    i++;
 }
 console.log(listGroupElement);
